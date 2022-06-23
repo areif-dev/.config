6893933687c60b3616677583b5e8e8acd9d49d8d
AJ Reifsnyder <ajreifsnyder@protonmail.com>
2022-06-22 21:56:10 -0400

    Changed mod key to super on master

--- qtile/config.py
+++ qtile/config.py
@@ -29,7 +29,7 @@ from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal
 
-mod = "mod1"  # Set mod key to Alt
+mod = "mod4"  # Set mod key to Alt
 terminal = guess_terminal()
 
 keys = [
