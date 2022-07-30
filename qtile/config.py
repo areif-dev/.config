fb22e5560df48ece61e983e1c05b9faf9f26ab87
areif-dev <ajreifsnyder@protonmail.com>
2022-07-30 13:42:00 -0400

    Switched qtile to column layout

--- qtile/config.py
+++ qtile/config.py
@@ -110,19 +110,27 @@ for i in groups:
     )
 
 layouts = [
-    # layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4),
+    layout.Columns(
+         margin=5,
+         border_width=3,
+         border_focus="#cba6f7",
+    ),
     # layout.Max(),
     # Try more layouts by unleashing below layouts.
     # layout.Stack(num_stacks=2),
     # layout.Bsp(),
     # layout.Matrix(),
-    # layout.MonadTall(),
+    # layout.MonadTall(
+    #     margin=10,
+    #     border_width=3,
+    #     border_focus="#cba6f7",
+    # ),
     # layout.MonadWide(),
-    layout.RatioTile(
-        margin=5,
-        border_width=3,
-        border_focus="#cba6f7",
-    ),
+    # layout.RatioTile(
+    #     margin=5,
+    #     border_width=3,
+    #     border_focus="#cba6f7",
+    # ),
     # layout.Tile(),
     # layout.TreeTab(),
     # layout.VerticalTile(),
